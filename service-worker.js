3ffddd8d4a21a7466ffe6f54c7a7191b9fa21efc
t6878472-ship-it <t6878472@gmail.com>
2026-06-12 17:54:07 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,18 @@
-const CACHE_NAME = "trombone-trainer-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("trombone-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "app.js",
+        "manifest.json"
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
